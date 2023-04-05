1435a3d8c6bc7a0334ef3f55433a64544e6f3fbf
Jan Latecka <jan@latecka.sk>
2023-04-05 17:51:46 +0200

    test

--- Neurologia/settings.py
+++ Neurologia/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-pv6%v-%p)hg%3e&wc$(4yl7t#h=*&$kyg4puu)ou*rqm55acu_'
 
-# SECURITY WARNING: don't run with debug turned on in production!
+# SECURITY WARNING: don't run with debug turned on in production!!
 DEBUG = True
 
 ALLOWED_HOSTS = []
